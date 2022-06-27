04652e6246df9f68f0f55f3206d62454424c9461
wahyuyulyartha <90485888+wahyuyuly@users.noreply.github.com>
2022-06-27 07:26:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  <link rel="icon" href="https://storage.cloud.google.com/zahra-bucket/img/header_logo.png">
+  <link rel="icon" href="https://storage.cloud.google.com/yuriko-bucket/img/header_logo.png">
   <link rel="stylesheet" type="text/css" href="style.css">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
@@ -64,7 +64,7 @@
 
   <!-- Header -->
   <header>
-    <img src="https://storage.cloud.google.com/zahra-bucket/img/header.PNG" >
+    <img src="https://storage.cloud.google.com/yuriko-bucket/img/header.PNG" >
   </header>
   <!-- Batas Header -->
 
